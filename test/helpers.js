832df511cee6c7b99bfe4b90551b2d5e6ca32b92
Omar Alshaker <omar@omaralshaker.com>
2018-01-04 10:45:12 +0100

    Enhance closeEnoughToValue assertion

--- test/helpers.js
+++ test/helpers.js
@@ -9,15 +9,20 @@ function simulateEvent(name, element) {
   }
 }
 
-chai.Assertion.addProperty('visible', function () {
-  this.assert(this._obj.offsetWidth || this._obj.offsetHeight || this._obj.getClientRects().length,
-      "expected element #{this} to be visible, but it's hidden",
-      "expected element #{this} to be hidden, but it's visible");
-})
+chai.Assertion.addProperty("visible", function() {
+  this.assert(
+    this._obj.offsetWidth ||
+      this._obj.offsetHeight ||
+      this._obj.getClientRects().length,
+    "expected element #{this} to be visible, but it's hidden",
+    "expected element #{this} to be hidden, but it's visible"
+  );
+});
 
-chai.Assertion.addMethod('closeEnoughToValue', function (value, distance = 5) {
-  debugger
-  this.assert(Math.abs(this._obj - value) < distance),
-      `expected element #{this} to be have max distance of ${distance} to ${value} but it has ${Math.abs(this._obj - value)}`,
-      `expected element #{this} to be have min distance of ${distance} to ${value} but it has ${Math.abs(this._obj - value)}`;
-})
+chai.Assertion.addMethod("closeEnoughToValue", function(value, distance = 5) {
+  const testedDistance = Math.abs(this._obj - value);
+  this.assert(
+    testedDistance < distance,
+    `expected #{this} to be have max distance of ${distance} to ${value} but it has ${testedDistance}`
+  );
+});
